bb8583d69c3ef76529e83946a691f59c84ef38a3
pantao <54778899@qq.com>
2018-12-14 13:52:27 +0800

    u03+09

--- 03.dart
+++ 03.dart
@@ -12,6 +12,9 @@ void main() {
   var isRight = false;
   print('isRight? ${isRight}');
 
+  // 可以在开发环境中使用 assert 函数，判断某个计算结果是否为 true，它只在 development 环境下会生效
+  assert(isRight == true);
+
   // list 型
   List<int> integerList = [1, 2, 3, 4];
   print('integerList is ${integerList}');
@@ -28,13 +31,36 @@ void main() {
   List<Object> objects = ['english', 'chinese', true, 10, () => print('func')];
   print('objects is ${objects}');
 
+  // 可以这样创建一个 map
   Map<String, String> keyValues = {
     'name': 'Pan Tao',
     'birthday': '1988-08-08',
     'address': 'Beijing, People\'s Republic of China'
   };
+  // 或者可以这样
+  var keyValues2 = {
+    'name': 'Pan Tao',
+    'birthday': '1988-08-08',
+    'address': 'Beijing, People\'s Republic of China'
+  };
+  // 或者也可以这样
+  var keyValues3 = Map();
+  keyValues3['name'] = 'Pan Tao';
+  keyValues3['birthdary'] = '1988-08-08';
+  keyValues3['address'] = 'Beijing, People\'s Republic of China';
   print('Map<String, String> keyValues: ${keyValues}');
+  print('Map<String, String> keyValues: ${keyValues2}');
+  print('Map<String, String> keyValues: ${keyValues3}');
+  print('keyValues 是否与 keyValues2 相等？${keyValues == keyValues2}');
+  print('keyValues2 是否与 keyValues3 相等？${keyValues2 == keyValues3}');
+  print('keyValues 是否与 keyValues3 相等？${keyValues == keyValues3}');
 
-  // 可以在开发环境中使用 assert 函数，判断某个计算结果是否为 true，它只在 development 环境下会生效
-  assert(isRight == true);
+  // 可以使用 const 创建一个不可变的 map
+  final constMap = const {
+    'name': 'Pan Tao'
+  };
+  print('constMap : ${constMap}');
+  // const map 是不能进行任何修改的
+  // constMap['name'] = 'Tao Pan';
+  // print('constMap : ${constMap}');
 }
